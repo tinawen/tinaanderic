a77470a67a1fbbbeb14f0bec50605903800c2833
Piotr Fidkowski <piotr@fidkowski.com>
2014-12-06 17:15:30 -0800

    fixed up navbar, added welcome page, added placeholder pages

--- app.py
+++ app.py
@@ -8,9 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.jinja2')
 
+@app.route("/rsvp")
+def rsvp():
+    return render_template('rsvp.jinja2')
+
 @app.route("/event")
 def event():
     return render_template('event.jinja2')
+
+@app.route("/registry")
+def registry():
+    return render_template('registry.jinja2')
+
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.jinja2')
+
+@app.route("/stories")
+def stories():
+    return render_template('stories.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
