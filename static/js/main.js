4d48d05da221b1eb3f85acd45491297cddc34ba3
Piotr Fidkowski <piotr@fidkowski.com>
2015-02-28 15:13:26 -0800

    add js based token submission, handle user input errors, handle bad token

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
 function submit_token() {
-  
+  var token = $("#token").val();
+  if (token.length != 6) {
+    $("#failure").show();
+  } else {
+    $("#failure").hide();
+    token = token.toUpperCase();
+    window.location.replace('rsvp/' + token)
+  }
 }
