71d12d2dad5c09d1a51583eeecc84be7af5bcd76
Piotr Fidkowski <piotr@fidkowski.com>
2015-02-28 15:25:43 -0800

    change .jinja2 -> .hmtl to enable autoescaping

--- app.py
+++ app.py
@@ -29,39 +29,39 @@ class Guest(db.Model):
 # -------------VIEWS-------------
 @app.route("/")
 def index():
-  return render_template('index.jinja2', active="home")
+  return render_template('index.html', active="home")
 
 @app.route("/rsvp")
 def rsvp():
-  return render_template('rsvp.jinja2', active="rsvp")
+  return render_template('rsvp.html', active="rsvp")
 
 @app.route("/rsvp/<token>")
 def rsvp_with_token(token):
   guests = Guest.query.filter_by(token=token).all()
   if guests:
-    return render_template('rsvp_with_token.jinja2', active="rsvp", guests=guests)
+    return render_template('rsvp_with_token.html', active="rsvp", guests=guests)
   else:
-    return render_template('rsvp_with_bad_token.jinja2', active="rsvp")
+    return render_template('rsvp_with_bad_token.html', active="rsvp")
 
 @app.route("/event")
 def event():
-  return render_template('event.jinja2', active="event")
+  return render_template('event.html', active="event")
 
 @app.route("/accommodation")
 def accommodation():
-  return render_template('accommodation.jinja2', active="accomodation")
+  return render_template('accommodation.html', active="accomodation")
 
 @app.route("/gallery")
 def gallery():
-  return render_template('gallery.jinja2', active="gallery")
+  return render_template('gallery.html', active="gallery")
 
 @app.route("/stories")
 def stories():
-  return render_template('stories.jinja2', active="stories")
+  return render_template('stories.html', active="stories")
 
 @app.route("/registry")
 def registry():
-	return render_template('registry.jinja2', active="registry")
+	return render_template('registry.html', active="registry")
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
