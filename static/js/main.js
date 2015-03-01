4a5198a0bb86e46e9c04bec9994da391052ebf19
Tina Wen <tdubs@tinawen.com>
2015-02-28 19:24:41 -0800

    hooked up rsvp to server

--- static/js/main.js
+++ static/js/main.js
@@ -42,27 +42,32 @@ $( document ).ready(function() {
 		if (attending) {
 			element.text("Attending");
 			parent = element.closest('.rsvp-guest-coming');
-			meal = parent.next('.rsvp-guest-meal').addClass('show');
-			meal.next('.rsvp-guest-dietary').addClass('show');
+			meal = parent.next().next('.rsvp-guest-meal').addClass('show');
+			meal.next().next('.rsvp-guest-dietary').addClass('show');
 		} else {
 			element.text("Attend");
 			parent = element.closest('.rsvp-guest-coming');
-			meal = parent.next('.rsvp-guest-meal').removeClass('show');
-			meal.next('.rsvp-guest-dietary').removeClass('show');
+			meal = parent.next().next('.rsvp-guest-meal').removeClass('show');
+			meal.next().next('.rsvp-guest-dietary').removeClass('show');
 		}
 	}
 
 	$('.rsvp-options-yes').click(function() {
 		updateAttendingOptions(true, $(this));
 		$(this).next('.rsvp-options-no').removeClass('active');
+		$(this).closest('.rsvp-guest-coming').next('input').val(1);
 	});
 	$('.rsvp-options-no').click(function() {
 		var attendElem = $(this).prev('.rsvp-options-yes').removeClass('active');
 		updateAttendingOptions(false, attendElem);
+		attendElem.closest('.rsvp-guest-coming').next('input').val(0);
 	});
 
-	$('.rsvp-guest-meal .dropdown-menu li').click(function() {
+	$('.rsvp-guest-meal .dropdown-menu li a').click(function() {
 		$(this).closest('.dropdown-menu').prev('.mealSelection').children('.choose').text($(this).text());
+		var tabIndex = $(this).attr("tabIndex");
+		$(this).closest('.rsvp-guest-meal').next('input').val(tabIndex);
+		event.preventDefault();
 	});
 });
 
