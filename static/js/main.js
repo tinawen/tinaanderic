db37df5927b1ddfdea47b504779ca213c501069c
Tina Wen <tdubs@tinawen.com>
2015-02-28 16:57:49 -0800

    added more to rsvp

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,29 @@ function submit_token() {
     window.location.replace('rsvp/' + token)
   }
 }
+
+$( document ).ready(function() {
+	function updateAttendingOptions(attending, element) {
+		if (attending) {
+			element.text("Attending");
+			parent = element.closest('.rsvp-guest-coming');
+			meal = parent.next('.rsvp-guest-meal').addClass('show');
+			meal.next('.rsvp-guest-dietary').addClass('show');
+		} else {
+			element.text("Attend");
+			parent = element.closest('.rsvp-guest-coming');
+			meal = parent.next('.rsvp-guest-meal').removeClass('show');
+			meal.next('.rsvp-guest-dietary').removeClass('show');
+		}
+	}
+
+	$('.rsvp-options-yes').click(function() {
+		updateAttendingOptions(true, $(this));
+		$(this).next('.rsvp-options-no').removeClass('active');
+	});
+	$('.rsvp-options-no').click(function() {
+		var attendElem = $(this).prev('.rsvp-options-yes').removeClass('active');
+		updateAttendingOptions(false, attendElem);
+	});
+});
+
